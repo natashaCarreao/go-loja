f274c3b22718f7482053625a8d85442f424a4f66
Natasha Carreão <nta.13@live.com>
2022-05-12 22:32:53 -0300

    Query de select all

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/natashaCarreao/go-loja
+
+go 1.18
+
+require github.com/lib/pq v1.10.5
--- main.go
+++ main.go
@@ -1,38 +1,66 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
-	_"github.com/lib/pq"
+
+	_ "github.com/lib/pq"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	db := conectaBanco()
-	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{"Camiseta", "Preta", 200, 2},
-		{"Fone", "Otimo", 240, 25},
-		{"Tenis", "Preto e branco", 100, 12},
+	db := conectaBanco()
+
+	selectAll, err := db.Query("Select * from produtos")
+
+	if err != nil {
+		panic(err.Error())
 	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectAll.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectAll.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Id = id
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+	}
+
 	temp.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
 }
 
 type Produto struct {
+	Id              int
 	Nome, Descricao string
 	Preco           float64
 	Quantidade      int
 }
 
 func conectaBanco() *sql.DB {
-	conexao := "user=postgres dbname=goloja password=postgres host=postgres-db sslmode=desable"
-	db, err sql.Open("postgres", conexao)
+	conexao := "user=postgres dbname=goloja password=postgres host=postgres-db port=5432 sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
 
 	if err != nil {
 		panic(err.Error())
